5b5eb92018dc6bb81cbb2345fa47b229ff2a833d
Djibril <najidjibril@gmail.com>
2024-07-01 23:52:33 +0200

    chore : fix comments

--- src/components/customs/Layout/Navbar.tsx
+++ src/components/customs/Layout/Navbar.tsx
@@ -8,11 +8,11 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between bg-gray-300 p-3">
-      <Link href={routes.home()}>Logo</Link>
+      <Link href={routes.home}>Logo</Link>
       <div className="flex items-center gap-4">
-        <Link href={routes.home()}>{t("signup")}</Link>
+        <Link href={routes.home}>{t("signup")}</Link>
         <Link
-          href={routes.home()}
+          href={routes.home}
           className="rounded-full bg-slate-400 px-4 py-1"
         >
           {t("login")}
--- src/web/routes.ts
+++ src/web/routes.ts
@@ -1,5 +1,5 @@
 const routes = {
-  home: () => "/",
+  home: "/",
   api: {},
 }
 
