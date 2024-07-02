a5e6c414a0a85d719ba25db2a850b54063db69a8
Vincent Raoul <44976231+Limerio@users.noreply.github.com>
2024-07-02 13:05:53 +0200

    feat: create base create-companies page

--- src/web/routes.ts
+++ src/web/routes.ts
@@ -1,8 +1,15 @@
 const routes = {
   home: "/",
+  registration: "/registration",
+  freelance: {
+    createProfile: "/freelance/create-profile",
+  },
   api: {
     areas: {
       index: "/areas",
+    },
+    freelance: {
+      createAccount: "/freelance/create-account",
     },
     companies: {
       create: "/companies/create",
