c8b83a0ae8a1ee005e7fe25c843e322ddc414ea0
Vincent Raoul <44976231+Limerio@users.noreply.github.com>
2024-07-02 16:25:27 +0200

    chore: update create company api route

--- src/app/api/companies/create/route.ts
+++ src/app/api/companies/create/route.ts
@@ -2,20 +2,34 @@ import { z } from "zod"
 
 import { db } from "@/db/client"
 import { company } from "@/db/schema"
+import type { ReadonlyArrayZod } from "@/utils/types"
+import { companiesCreateFormValidator } from "@/validators/companies"
 
 const companyCreateValidator = z.object({
   businessName: z.string().min(1),
   description: z.string().min(1),
   areaId: z.string().uuid(),
+  logo: z.string().url(),
   headQuarter: z.string().min(1),
   kbisUrl: z.string().url(),
 })
 
 export const POST = async (req: Request) => {
   try {
-    const body = await companyCreateValidator.parseAsync(await req.json())
+    const areas = await db.query.area.findMany()
+    const body = await companiesCreateFormValidator(
+      areas.map((area) => area.value) as unknown as ReadonlyArrayZod,
+    ).parseAsync(await req.json())
 
-    await db.insert(company).values(body)
+    const data = await companyCreateValidator.parseAsync({
+      businessName: body.businessName,
+      description: body.descriptionCompany,
+      areaId: body.areaId,
+      headQuarter: body.headquarters,
+      kbisUrl: body.kibs,
+    })
+
+    await db.insert(company).values(data)
 
     return Response.json({}, { status: 201 })
   } catch (err) {
