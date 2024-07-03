57fe83ab13e0ef076e0dca2a83ba490819019438
Vincent Raoul <44976231+Limerio@users.noreply.github.com>
2024-07-03 16:31:47 +0200

    fix: next config, and companies page

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,15 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
 }
 
 export default withNextIntl(nextConfig)
--- src/app/[locale]/companies/[id]/page.tsx
+++ src/app/[locale]/companies/[id]/page.tsx
@@ -19,15 +19,21 @@ const CompaniesIdPage = () => {
   })
 
   const { data: areaResult } = useQuery<ApiResponse<SelectArea>>({
-    queryKey: [routes.api.areas[":id"](companyResult!.data.areaId)],
+    queryKey: [routes.api.areas[":id"](companyResult?.data.areaId)],
+    queryFn: () =>
+      apiFetch({
+        url: routes.api.areas[":id"](companyResult?.data.areaId),
+      }),
   })
 
   return (
     <div className="mt-6 flex flex-col items-center gap-5">
       <div className="grid grid-cols-2">
         <Image
-          src={companyResult!.data.logo}
+          src={companyResult?.data.logo}
           alt={`Logo of the company ${companyResult?.data.businessName}`}
+          width={640}
+          height={480}
         />
         <div className="flex flex-col items-center gap-3">
           <h1>{companyResult?.data.businessName}</h1>
