ed53bf4b62841f02787014df58aec87a9fb2fcdf
Djibril <najidjibril@gmail.com>
2024-07-03 15:21:02 +0200

    SH2-5: fix merge conflict

--- src/web/routes.ts
+++ src/web/routes.ts
@@ -2,9 +2,11 @@ const routes = {
   home: "/",
   login: "/login",
   registration: "/registration",
-  freelanceCreateProfile: "/freelance/create-profile",
-  freelanceUpdateProfile: (name: string, id: string) =>
-    `/freelance/update-profile/${name}?id=${id}`,
+  freelance: {
+    createProfile: "/freelance/create-profile",
+    updateProfile: (name: string, id: string) =>
+      `/freelance/update-profile/${name}?id=${id}`,
+  },
   profile: (name: string, id: string) => `/freelance/profile/${name}?id=${id}`,
   api: {
     auth: {
@@ -13,12 +15,24 @@ const routes = {
       logout: "/auth/logout",
       register: { freelance: "/auth/register/freelance" },
     },
-    updateAccount: (userId: string) => `/freelance/${userId}/account`,
-    areas: "/areas",
+    areas: {
+      index: "/areas",
+    },
     freelance: {
-      getProfile: (userId: string | null) => `/freelance/${userId}`,
+      createAccount: "/freelance/create-account",
+    },
+    companies: {
+      create: "/companies/create",
+      createAccount: "/freelance/create-account",
+      updateAccount: (userId: string) => `/freelance/${userId}/account`,
+      areas: "/areas",
+      freelance: {
+        getProfile: (userId: string | null) => `/freelance/${userId}`,
+      },
     },
   },
 } as const
+
+export type Routes = typeof routes
 
 export default routes
