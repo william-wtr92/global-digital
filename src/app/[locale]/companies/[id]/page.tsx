2535eb944e014f9650e77228c7201d97991926ed
Vincent Raoul <44976231+Limerio@users.noreply.github.com>
2024-07-04 11:16:41 +0200

    chore: update view company page, and create areas/[id] api

--- src/app/[locale]/companies/[id]/page.tsx
+++ src/app/[locale]/companies/[id]/page.tsx
@@ -3,43 +3,85 @@
 import { useQuery } from "@tanstack/react-query"
 import Image from "next/image"
 import { useParams } from "next/navigation"
+import { useTranslations } from "next-intl"
+import { toast } from "sonner"
 
+import Spinner from "@/components/customs/Utils/Spinner"
 import type { SelectArea, SelectCompany } from "@/db/schema"
+import { SC } from "@/def/status"
 import { apiFetch, type ApiResponse } from "@/lib/api"
+import { capitalizeFirstLetter } from "@/utils/forms"
 import routes from "@/web/routes"
 
 const CompaniesIdPage = () => {
+  const t = useTranslations("CompaniesId")
   const { id } = useParams()
-  const { data: companyResult } = useQuery<ApiResponse<SelectCompany>>({
-    queryKey: [routes.companies[":id"](id as string)],
-    queryFn: () =>
-      apiFetch({
+  const {
+    data: companyResult,
+    isSuccess,
+    isLoading: isLoadingCompany,
+  } = useQuery<ApiResponse<SelectCompany>>({
+    queryKey: [routes.api.companies[":id"](id as string)],
+    queryFn: async () => {
+      const response = await apiFetch({
         url: routes.api.companies[":id"](id as string),
-      }),
+      })
+
+      if (response.status !== SC.success.OK) {
+        toast.error(t("Form.error"))
+      }
+
+      return response
+    },
   })
 
-  const { data: areaResult } = useQuery<ApiResponse<SelectArea>>({
-    queryKey: [routes.api.areas[":id"](companyResult?.data.areaId)],
-    queryFn: () =>
-      apiFetch({
-        url: routes.api.areas[":id"](companyResult?.data.areaId),
-      }),
+  const { data: areaResult, isLoading: isLoadingArea } = useQuery<
+    ApiResponse<SelectArea>
+  >({
+    enabled: isSuccess,
+    queryKey: [routes.api.areas[":id"](companyResult?.data.areaId as string)],
+    queryFn: async () => {
+      const response = await apiFetch({
+        url: routes.api.areas[":id"](companyResult?.data.areaId as string),
+      })
+
+      if (response.status !== SC.success.OK) {
+        toast.error(t("Form.error"))
+      }
+
+      return response
+    },
   })
+
+  if (isLoadingCompany || isLoadingArea) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner />
+      </div>
+    )
+  }
 
   return (
     <div className="mt-6 flex flex-col items-center gap-5">
-      <div className="grid grid-cols-2">
+      <div className="flex items-center justify-center gap-5">
         <Image
-          src={companyResult?.data.logo}
-          alt={`Logo of the company ${companyResult?.data.businessName}`}
-          width={640}
-          height={480}
+          src={companyResult!.data.logo}
+          alt={`Logo of the company ${companyResult!.data.businessName}`}
+          width={360}
+          height={360}
+          className="rounded-md"
         />
         <div className="flex flex-col items-center gap-3">
-          <h1>{companyResult?.data.businessName}</h1>
+          <h1 className="text-5xl font-bold">
+            {companyResult?.data.businessName}
+          </h1>
           <h2>{companyResult?.data.headQuarter}</h2>
-          <h3>{areaResult?.data.value}</h3>
+          <h3>{capitalizeFirstLetter(areaResult!.data.value)}</h3>
         </div>
+      </div>
+      <p className="text-xl">{companyResult?.data.description}</p>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold">Missions disponibles</h1>
       </div>
     </div>
   )
--- src/app/api/areas/[id]/route.ts
+++ src/app/api/areas/[id]/route.ts
@@ -1,20 +1,17 @@
 import { eq } from "drizzle-orm"
 
 import { db } from "@/db/client"
-import { company } from "@/db/schema"
+import { area } from "@/db/schema"
 import { SC } from "@/def/status"
 
 export const GET = async (
-  req: Request,
+  request: Request,
   { params: { id } }: { params: { id: string } },
 ) => {
   try {
-    const companyData = await db
-      .select()
-      .from(company)
-      .where(eq(company.id, id!))
+    const areaData = await db.select().from(area).where(eq(area.id, id!))
 
-    return Response.json(companyData[0])
+    return Response.json(areaData[0])
   } catch (error) {
     return Response.json(
       { error },
