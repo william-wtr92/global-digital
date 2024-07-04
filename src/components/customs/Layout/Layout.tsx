9f70328e2c0907f33933d8ba976afcf104386ccd
Vincent Raoul <44976231+Limerio@users.noreply.github.com>
2024-07-04 14:49:57 +0200

    fix: review @william-wtr92

--- src/components/customs/Layout/Layout.tsx
+++ src/components/customs/Layout/Layout.tsx
@@ -3,23 +3,17 @@
 import { type ReactNode } from "react"
 
 import Navbar from "@/components/customs/Layout/Navbar"
-import { usePathname } from "@/navigation"
 
 type LayoutProps = {
   token: string | undefined
   children?: ReactNode
 }
 
-const Layout = ({ children, token }: LayoutProps) => {
-  const pathname = usePathname()
-  const isLoginPage = pathname.includes("/login")
-
-  return (
-    <div className="flex h-screen flex-col">
-      {!isLoginPage && <Navbar token={token} />}
-      {children}
-    </div>
-  )
-}
+const Layout = ({ children, token }: LayoutProps) => (
+  <div className="flex h-screen flex-col">
+    <Navbar token={token} />
+    {children}
+  </div>
+)
 
 export default Layout
