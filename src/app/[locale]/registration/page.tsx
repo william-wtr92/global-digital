590412e0fbab8684f543d4a2bd430fe54b2d9159
Djibril <najidjibril@gmail.com>
2024-07-05 12:01:08 +0200

    fix

--- src/app/[locale]/registration/page.tsx
+++ src/app/[locale]/registration/page.tsx
@@ -8,8 +8,8 @@ const RegistrationPage = () => {
 
   return (
     <div className="flex h-full flex-col items-center justify-center gap-20 py-10">
-      <h1 className="text-blueText text-4xl font-extrabold">{t("hello")}</h1>
-      <h2 className="text-blueText text-3xl font-bold">{t("whoAreYou")}</h2>
+      <h1 className="text-4xl font-extrabold text-blueText">{t("hello")}</h1>
+      <h2 className="text-3xl font-bold text-blueText">{t("whoAreYou")}</h2>
 
       <div className="flex flex-col gap-20 md:flex-row">
         <Link
