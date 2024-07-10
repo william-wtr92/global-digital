b02bf9fb082ded6a35c589cb4729face49b8b111
Vincent Raoul <44976231+Limerio@users.noreply.github.com>
2024-07-10 16:34:18 +0200

    fix: small lint bug

--- src/features/companies/employees/hooks/useCompanyEmployees.ts
+++ src/features/companies/employees/hooks/useCompanyEmployees.ts
@@ -3,10 +3,9 @@
 import type { User } from "@/features/account/types/user"
 import type { Employee } from "@/features/companies/employees/types/employee"
 import type { EmployeeRole } from "@/features/companies/employees/types/employeeRole"
+import type { Role } from "@/features/roles/types/role"
 import { useQuery } from "@/hooks/useQuery"
 import routes from "@/utils/routes"
-
-import type { Role } from "@/features/companies/employees/types/role"
 
 export type EmployeeResponse = {
   Role: Role
